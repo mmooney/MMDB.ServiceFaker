42ea815b808c2bd1908c8f4c28dc68ebb2e579f3
Mike Mooney <mmooney@mmdbsolutions.com>
2013-04-12 15:03:35 -0400

    reorganized DB project

--- MMDB.ServiceFaker.Database/deployment/LOCAL.DBDeployment.bat
+++ MMDB.ServiceFaker.Database/deployment/LOCAL.DBDeployment.bat
@@ -3,13 +3,10 @@
 SET DIR=%~d0%~p0%
 
 SET database.name="MMDBServiceFaker"
-SET sql.files.directory="%DIR%..\db\SQLServer\TestRoundhousE"
+SET sql.files.directory="%DIR%..\db"
 SET server.database="(local)"
-SET repository.path="http://roundhouse.googlecode.com/svn"
-SET version.file="_BuildInfo.xml"
-SET version.xpath="//buildInfo/version"
-SET environment="LOCAL"
+SET repository.path="http://roundhouse.googlecode.com/svn"SET environment="LOCAL"
 
-"%DIR%Console\rh.exe" /d=%database.name% /f=%sql.files.directory% /s=%server.database% /vf=%version.file% /vx=%version.xpath% /r=%repository.path% /env=%environment% /simple
+"%DIR%Console\rh.exe" /d=%database.name% /f=%sql.files.directory% /s=%server.database% /env=%environment% /simple
 
 pause
